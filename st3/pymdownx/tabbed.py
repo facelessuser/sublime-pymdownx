463c6fc7b525b2f1e55d3a4bc78aa2ccf0aeaf46
facelessuser <faceless.shop@gmail.com>
2021-02-14 10:39:08 -0700

    Update to latest version

--- st3/pymdownx/tabbed.py
+++ st3/pymdownx/tabbed.py
@@ -1,7 +1,7 @@
 """
-Details.
+Tabbed.
 
-pymdownx.details
+pymdownx.tabbed
 
 MIT license.
 
@@ -27,24 +27,24 @@ import xml.etree.ElementTree as etree
 import re
 
 
-class DetailsProcessor(BlockProcessor):
-    """Details block processor."""
+class TabbedProcessor(BlockProcessor):
+    """Tabbed block processor."""
 
     START = re.compile(
-        r'(?:^|\n)\?{3}(\+)? ?(?:([\w\-]+(?: +[\w\-]+)*?)?(?: +"(.*?)")|([\w\-]+(?: +[\w\-]+)*?)) *(?:\n|$)'
+        r'(?:^|\n)={3}(!)? +"(.*?)" *(?:\n|$)'
     )
     COMPRESS_SPACES = re.compile(r' {2,}')
 
-    def __init__(self, parser):
-        """Initialization."""
+    def __init__(self, *args):
+        """Initialize."""
 
-        super().__init__(parser)
-
+        super().__init__(*args)
+        self.tab_group_count = 0
         self.current_sibling = None
         self.content_indention = 0
 
     def get_sibling(self, parent, block):
-        """Get sibling details.
+        """Get sibling tab.
 
         Retrieve the appropriate sibling element. This can get tricky when
         dealing with lists.
@@ -61,23 +61,39 @@ class DetailsProcessor(BlockProcessor):
 
         sibling = self.lastChild(parent)
 
-        if sibling is None or sibling.tag.lower() != 'details':
+        if sibling is None or sibling.tag.lower() != 'div' or sibling.attrib.get('class', '') != 'tabbed-set':
             sibling = None
         else:
             # If the last child is a list and the content is indented sufficient
             # to be under it, then the content's is sibling is in the list.
             last_child = self.lastChild(sibling)
+            child_class = last_child.attrib.get('class', '') if last_child else ''
             indent = 0
             while last_child:
                 if (
                     sibling and block.startswith(' ' * self.tab_length * 2) and
-                    last_child and last_child.tag in ('ul', 'ol', 'dl')
+                    last_child and (
+                        last_child.tag in ('ul', 'ol', 'dl') or
+                        (
+                            last_child.tag == 'div' and
+                            child_class in ('tabbed-content',)
+                        )
+                    )
                 ):
+
+                    # Handle nested tabbed content
+                    if last_child.tag == 'div' and child_class == 'tabbed-content':
+                        temp_child = self.lastChild(last_child)
+                        if temp_child.tag not in ('ul', 'ol', 'dl'):
+                            break
+                        last_child = temp_child
+                        child_class = last_child.attrib.get('class', '') if last_child else ''
 
                     # The expectation is that we'll find an `<li>`.
                     # We should get it's last child as well.
                     sibling = self.lastChild(last_child)
                     last_child = self.lastChild(sibling) if sibling else None
+                    child_class = last_child.attrib.get('class', '') if last_child else ''
 
                     # Context has been lost at this point, so we must adjust the
                     # text's indentation level so it will be evaluated correctly
@@ -105,67 +121,117 @@ class DetailsProcessor(BlockProcessor):
             return self.get_sibling(parent, block)[0] is not None
 
     def run(self, parent, blocks):
-        """Convert to details/summary block."""
+        """Convert to tabbed block."""
 
         block = blocks.pop(0)
         m = self.START.search(block)
 
         if m:
-            # remove the first line
+            # removes the first line
             if m.start() > 0:
                 self.parser.parseBlocks(parent, [block[:m.start()]])
             block = block[m.end():]
+            sibling = self.lastChild(parent)
         else:
             sibling, block = self.get_sibling(parent, block)
 
-        # Get the details block and and the non-details content
-        block, non_details = self.detab(block)
+        # Get the tabs block and the non-tab content
+        block, non_tabs = self.detab(block)
 
         if m:
-            state = m.group(1)
-            is_open = state is not None
+            special = m.group(1) if m.group(1) else ''
+            title = m.group(2) if m.group(2) else m.group(3)
 
-            if m.group(4):
-                class_name = self.COMPRESS_SPACES.sub(' ', m.group(4).lower())
-                title = class_name.split(' ')[0].capitalize()
+            if (
+                sibling and sibling.tag.lower() == 'div' and
+                sibling.attrib.get('class', '') == 'tabbed-set' and
+                special != '!'
+            ):
+                first = False
+                sfences = sibling
             else:
-                classes = m.group(2)
-                class_name = '' if classes is None else self.COMPRESS_SPACES.sub(' ', classes.lower())
-                title = m.group(3)
+                first = True
+                self.tab_group_count += 1
+                sfences = etree.SubElement(
+                    parent,
+                    'div',
+                    {'class': 'tabbed-set', 'data-tabs': '%d:0' % self.tab_group_count}
+                )
 
-            div = etree.SubElement(parent, 'details', ({'open': 'open'} if is_open else {}))
-            if class_name:
-                div.set('class', class_name)
-            summary = etree.SubElement(div, 'summary')
-            summary.text = title
+            data = sfences.attrib['data-tabs'].split(':')
+            tab_set = int(data[0])
+            tab_count = int(data[1]) + 1
+
+            attributes = {
+                "name": "__tabbed_%d" % tab_set,
+                "type": "radio",
+                "id": "__tabbed_%d_%d" % (tab_set, tab_count)
+            }
+
+            if first:
+                attributes['checked'] = 'checked'
+
+            etree.SubElement(
+                sfences,
+                'input',
+                attributes
+            )
+            lab = etree.SubElement(
+                sfences,
+                "label",
+                {
+                    "for": "__tabbed_%d_%d" % (tab_set, tab_count)
+                }
+            )
+            lab.text = title
+
+            div = etree.SubElement(
+                sfences,
+                "div",
+                {
+                    "class": "tabbed-content"
+                }
+            )
+            sfences.attrib['data-tabs'] = '%d:%d' % (tab_set, tab_count)
         else:
-            # Sibling is a list item, but we need to wrap it's content should be wrapped in <p>
             if sibling.tag in ('li', 'dd') and sibling.text:
+                # Sibling is a list item, but we need to wrap it's content should be wrapped in <p>
                 text = sibling.text
                 sibling.text = ''
                 p = etree.SubElement(sibling, 'p')
                 p.text = text
-
-            div = sibling
+                div = sibling
+            elif sibling.tag == 'div' and sibling.attrib.get('class', '') == 'tabbed-set':
+                # Get `tabbed-content` under `tabbed-set`
+                div = self.lastChild(sibling)
+            else:
+                # Pass anything else as the parent
+                div = sibling
 
         self.parser.parseChunk(div, block)
 
-        if non_details:
-            # Insert the non-details content back into blocks
-            blocks.insert(0, non_details)
+        if non_tabs:
+            # Insert the tabbed content back into blocks
+            blocks.insert(0, non_tabs)
 
 
-class DetailsExtension(Extension):
-    """Add Details extension."""
+class TabbedExtension(Extension):
+    """Add Tabbed extension."""
 
     def extendMarkdown(self, md):
-        """Add Details to Markdown instance."""
+        """Add Tabbed to Markdown instance."""
         md.registerExtension(self)
 
-        md.parser.blockprocessors.register(DetailsProcessor(md.parser), "details", 105)
+        self.tab_processor = TabbedProcessor(md.parser)
+        md.parser.blockprocessors.register(self.tab_processor, "tabbed", 105)
+
+    def reset(self):
+        """Reset."""
+
+        self.tab_processor.tab_group_count = 0
 
 
 def makeExtension(*args, **kwargs):
     """Return extension."""
 
-    return DetailsExtension(*args, **kwargs)
+    return TabbedExtension(*args, **kwargs)
